0b634b823683f328ff0cdd302059fb8c3acf01b3
alkpo1837 <capo.arthur@gmail.com>
2020-09-17 10:38:20 +0200

    Code clean & fix some countries

--- Unity_Project/Assets/Rcs/Scripts/StatsPoster.cs
+++ Unity_Project/Assets/Rcs/Scripts/StatsPoster.cs
@@ -59,7 +59,6 @@ public class StatsPoster : MonoBehaviour
 
         if (fillAmount < 1.0f)
         {
-            Debug.LogWarningFormat("Filling {0} with fillAMount {1}", country, fillAmount);
             flagIconBehaviour.SetFillAmount(fillAmount, fillOrigin);
         }
 
@@ -81,106 +80,18 @@ public class StatsPoster : MonoBehaviour
         worldFlagIconGo.transform.localPosition = new Vector2(posX, posY);
         worldFlagIconBehaviour.SetSize(NbrColumns - currCol);
 
-        // Set Size
-
         worldFlagIconGo.name = string.Format("World: {0}", 100.0f - totalPercentage);
     }
 
-    //public void FillFlags(List<JSONObject> listStats)
-    //{
-    //    int currLine = 0, currCol = 0;
-    //    float totalPercentage = 0.0f;
-    //    float currPercentage = 0.0f;
-    //    int currIndex = 0;
-
-    //    if (_flagsIcon.Count > 0)
-    //    {
-    //        foreach (FlagIcon flagIcon in _flagsIcon)
-    //            Destroy(flagIcon.gameObject);
-
-    //        _flagsIcon.Clear();
-    //    }
-
-    //    Debug.LogFormat("_ratioPerFlag = {0}", _ratioPerFlag);
-
-    //    foreach (JSONObject statJSON in listStats)
-    //    {
-    //        string country = statJSON.keys[0];
-    //        float percentage = statJSON[country].f;
-
-    //        Debug.LogWarningFormat("We got {0} with {1} %", country, percentage);
-
-    //        if (percentage > 1.0f)
-    //        {
-    //            for (; currPercentage < percentage + DECIMAL_MARGIN; currPercentage += _ratioPerFlag)
-    //            {
-    //                currCol = Mathf.FloorToInt(currIndex) % NbrColumns;
-    //                currLine = (int)Mathf.Floor(Mathf.FloorToInt(currIndex) / NbrColumns);
-
-    //                Debug.LogFormat("CurrIndex => {0}", currIndex);
-    //                Debug.LogFormat("CurrCol = {0}, CurrLine = {1}", currCol, currLine);
-    //                Debug.Log("CurrPercentage is " + currPercentage);
-
-    //                if (currPercentage + _ratioPerFlag > percentage + DECIMAL_MARGIN)
-    //                {
-    //                    Debug.LogWarningFormat("At {0} ({1}) we should not fill completely", currPercentage, percentage);
-
-    //                    InstantiateFlag(country, currCol, currLine, percentage - currPercentage, percentage - currPercentage);
-
-    //                    totalPercentage += percentage - currPercentage;
-
-    //                    break;
-    //                }
-    //                else
-    //                {
-    //                    InstantiateFlag(country, currCol, currLine, percentage);
-
-    //                    totalPercentage += _ratioPerFlag;
-    //                    currIndex++;
-    //                }
-    //            }
-
-    //            Debug.LogErrorFormat("TotalPercentage {0}, TotalPer / ratioPerFlag is {1}", totalPercentage, totalPercentage % _ratioPerFlag);
-
-    //            if (totalPercentage % _ratioPerFlag > DECIMAL_MARGIN)
-    //            {
-    //                float fillAmount = 1.0f - (totalPercentage % _ratioPerFlag);
-    //                Debug.LogError("FILLING LEFT PART");
-    //                InstantiateFlag(country, currCol, currLine, percentage, fillAmount, 1);
-
-    //                // This is the percentage we're curently at
-    //                currPercentage = fillAmount;
-    //                totalPercentage += fillAmount;
-
-    //                currIndex++;
-    //            }
-    //            else
-    //            {
-    //                currPercentage = 0.0f;
-    //            }
-    //        }
-    //        else
-    //        {
-    //            //If percentage< 1.0f, we fill with the world icon
-
-    //            InstantiateWorldFlag(currCol, currLine, totalPercentage);
-
-    //            break;
-    //        }
-    //    }
-    //}
-
     public void FillFlags(List<JSONObject> listStats)
     {
-        if (_flagsIcon.Count > 0)
-        {
-            foreach (FlagIcon flagIcon in _flagsIcon)
-                Destroy(flagIcon.gameObject);
-
-            _flagsIcon.Clear();
-        }
-
+        int currLine = 0, currCol = 0;
+        float currFlagFill = 0.0f;
+        int currIndex = 0;
         Dictionary<string, float> nbrFlagsByCountries = new Dictionary<string, float>();
+        float flagsRemaining = 100.0f;
+
+        ClearAllFlags();
 
         foreach (JSONObject statJSON in listStats)
         {
@@ -188,13 +99,11 @@ public class StatsPoster : MonoBehaviour
             float percentage = statJSON[country].f;
 
             if (percentage > 0.1f)
+            {
                 nbrFlagsByCountries[country] = (NbrColumns * NbrLines) * (percentage / 100.0f);
+                flagsRemaining -= nbrFlagsByCountries[country];
+            }
         }
-
-        int currLine = 0, currCol = 0;
-        float totalPercentage = 0.0f;
-        float currFlagFill = 0.0f;
-        int currIndex = 0;
 
         foreach (KeyValuePair<string, float> nbrFlagByCountry in nbrFlagsByCountries)
         {
@@ -208,11 +117,7 @@ public class StatsPoster : MonoBehaviour
                 InstantiateFlag(nbrFlagByCountry.Key, currCol, currLine, nbrFlagByCountry.Value, 1 - currFlagFill, 1);
                 currIndex++;
 
-                Debug.Log("Previous currFlagFill is " + currFlagFill);
-
                 nbrFlags -= (1 - currFlagFill);
-
-                Debug.Log(nbrFlags);
             }
 
             for (int i = 0; i < Mathf.Floor(nbrFlags); i++)
@@ -222,90 +127,37 @@ public class StatsPoster : MonoBehaviour
 
                 InstantiateFlag(nbrFlagByCountry.Key, currCol, currLine, nbrFlagByCountry.Value);
 
-                totalPercentage += i;
                 currIndex++;
-
-                Debug.Log(i + " vs " + nbrFlags);
-
                 currFlagFill = 1.0f;
             }
 
             // If there is a decimal part left, we should add this decimal part to the next flag icon
             if (nbrFlags - Math.Truncate(nbrFlags) > DECIMAL_MARGIN)
             {
-                currFlagFill = nbrFlags - (float) Math.Truncate(nbrFlags);
+                currFlagFill = nbrFlags - (float)Math.Truncate(nbrFlags);
 
                 currCol = Mathf.FloorToInt(currIndex) % NbrColumns;
                 currLine = (int)Mathf.Floor(Mathf.FloorToInt(currIndex) / NbrColumns);
 
-                //Debug.LogWarningFormat("At {0} ({1}) we should not fill completely", currPercentage, percentage);
-
                 InstantiateFlag(nbrFlagByCountry.Key, currCol, currLine, nbrFlagByCountry.Value, currFlagFill);
             }
-
-            //if (nbrFlagByCountry.Value > i)
         }
 
-        //Debug.LogWarningFormat("We got {0} with {1} %", country, percentage);
+        if (flagsRemaining > DECIMAL_MARGIN)
+        {
+            Debug.Log("There is still " + flagsRemaining + " flags remaining");
+        }
+    }
 
-        //if (percentage > 1.0f)
-        //{
-        //    for (; currPercentage < percentage + DECIMAL_MARGIN; currPercentage += _ratioPerFlag)
-        //    {
-        //        currCol = Mathf.FloorToInt(currIndex) % NbrColumns;
-        //        currLine = (int)Mathf.Floor(Mathf.FloorToInt(currIndex) / NbrColumns);
+    private void ClearAllFlags()
+    {
+        if (_flagsIcon.Count > 0)
+        {
+            foreach (FlagIcon flagIcon in _flagsIcon)
+                Destroy(flagIcon.gameObject);
 
-        //        Debug.LogFormat("CurrIndex => {0}", currIndex);
-        //        Debug.LogFormat("CurrCol = {0}, CurrLine = {1}", currCol, currLine);
-        //        Debug.Log("CurrPercentage is " + currPercentage);
-
-        //        if (currPercentage + _ratioPerFlag > percentage + DECIMAL_MARGIN)
-        //        {
-        //            Debug.LogWarningFormat("At {0} ({1}) we should not fill completely", currPercentage, percentage);
-
-        //            InstantiateFlag(country, currCol, currLine, percentage - currPercentage, percentage - currPercentage);
-
-        //            totalPercentage += percentage - currPercentage;
-
-        //            break;
-        //        }
-        //        else
-        //        {
-        //            InstantiateFlag(country, currCol, currLine, percentage);
-
-        //            totalPercentage += _ratioPerFlag;
-        //            currIndex++;
-        //        }
-        //    }
-
-        //    Debug.LogErrorFormat("TotalPercentage {0}, TotalPer / ratioPerFlag is {1}", totalPercentage, totalPercentage % _ratioPerFlag);
-
-        //    if (totalPercentage % _ratioPerFlag > DECIMAL_MARGIN)
-        //    {
-        //        float fillAmount = 1.0f - (totalPercentage % _ratioPerFlag);
-        //        Debug.LogError("FILLING LEFT PART");
-        //        InstantiateFlag(country, currCol, currLine, percentage, fillAmount, 1);
-
-        //        // This is the percentage we're curently at
-        //        currPercentage = fillAmount;
-        //        totalPercentage += fillAmount;
-
-        //        currIndex++;
-        //    }
-        //    else
-        //    {
-        //        currPercentage = 0.0f;
-        //    }
-        //}
-        //else
-        //{
-        //    //If percentage< 1.0f, we fill with the world icon
-
-        //    InstantiateWorldFlag(currCol, currLine, totalPercentage);
-
-        //    break;
-        //}
-
+            _flagsIcon.Clear();
+        }
     }
 
 
